e5f39d1f1b6724f13987bf1ceca2e70189db9e49
“Andrew <andrewwoods88@gmail.com>
2019-07-16 14:36:38 -0400

    created nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => {:registrations => "registrations"}
   
-  resources :tools
-  resources :topics
+  resources :topics do
+    scope module: :topics do
+      resources :tools 
+    end
+  end
 
   get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
